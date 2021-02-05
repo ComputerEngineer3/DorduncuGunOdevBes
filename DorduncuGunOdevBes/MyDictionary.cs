ecbd81eea5d91d064f468ecc373250fdd37d98d6
ComputerEngineer3 <aynr.fsjf@outlook.com>
2021-02-05 22:12:31 +0300

    Add project files.

---
+++ DorduncuGunOdevBes/MyDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DorduncuGunOdevBes
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] tKeys;
+        TValue[] tValues;
+        public MyDictionary()
+        {
+            tKeys = new TKey[0];
+            tValues = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempArrayKey = tKeys;
+            TValue[] tempArrayValue = tValues;
+
+            tKeys = new TKey[tKeys.Length + 1];
+            tValues = new TValue[tValues.Length + 1];
+
+            for (int i = 0; i < tempArrayKey.Length; i++)
+            {
+                tKeys[i] = tempArrayKey[i];
+            }
+
+            for (int i = 0; i < tempArrayValue.Length; i++)
+            {
+                tValues[i] = tempArrayValue[i];
+            }
+
+            tKeys[tKeys.Length - 1] = key;
+            tValues[tValues.Length - 1] = value;
+
+        }
+    }
+}
---
+++ DorduncuGunOdevBes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DorduncuGunOdevBes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            MyDictionary<int, int> myDictionary1 = new MyDictionary<int, int>();
+            myDictionary1.Add(1, 2);
+            myDictionary1.Add(3, 4);
+            myDictionary1.Add(5, 6);
+
+            MyDictionary<int, string> myDictionary2 = new MyDictionary<int, string>();
+            myDictionary2.Add(1, "one");
+            myDictionary2.Add(2, "two");
+            myDictionary2.Add(3, "three");
+        }
+    }
+}
